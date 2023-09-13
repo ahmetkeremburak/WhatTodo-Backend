1d946b7be91d408846f3ead5881be57053488294
Ahmet Kerem Burak <keremburak00@gmail.com>
2023-09-13 14:22:04 +0300

    project base loaded

---
+++ src/test/java/com/ahmetkeremburak/whattodo/WhattodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetkeremburak.whattodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
