500f793de6f91dae68cb922749f9efe664edf80e
Ahmet Kerem Burak <keremburak00@gmail.com>
2023-09-23 21:15:38 +0300

    update | todo item convention

--- src/main/java/com/ahmetkeremburak/whattodo/model/TodoItem.java
+++ src/main/java/com/ahmetkeremburak/whattodo/model/TodoItem.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "todoitems")
 public class TodoItem {
 
     @Id
@@ -12,7 +13,7 @@ public class TodoItem {
     private Long id;
 
     @Column(nullable = false)
-    private String Task;
+    private String task;
 
     @Column(nullable = false)
     private boolean isChecked;
