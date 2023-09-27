d80210823f7b6e926343061111d0c7e20c992afc
Ahmet Kerem Burak <keremburak00@gmail.com>
2023-09-27 19:49:33 +0300

    add | repo method to find item by TodoState

--- src/main/java/com/ahmetkeremburak/whattodo/repository/TodoRepo.java
+++ src/main/java/com/ahmetkeremburak/whattodo/repository/TodoRepo.java
@@ -1,10 +1,13 @@
 package com.ahmetkeremburak.whattodo.repository;
 
 import com.ahmetkeremburak.whattodo.model.TodoItem;
+import com.ahmetkeremburak.whattodo.model.TodoState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepo extends JpaRepository<TodoItem, Long> {
-
+    List<TodoItem> findByTodoState(TodoState state);
 }
